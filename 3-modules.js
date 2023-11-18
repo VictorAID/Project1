55e31bb9ac25369fdeb508467cd8b3781b5c0ca7
VictorCode <victoranalyst01@gmail.com>
2023-11-18 16:08:03 +0000

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+const {john,peter} = require("./5-utils")
+const sayHi = require("./4-names")
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+// console.log(data)
+// sayHi(john)
+// sayHi(peter)
+//module.exports = {john, peter}
+
+ 
---
+++ 8-os-module.js
@@ -0,0 +1,13 @@
+const os = require('os');
+//info about current user
+
+const user = os.userInfo();
+const time = os.uptime();
+const  currentOS = {
+    name: os.type(), 
+    release:os.release(),
+    totalMem:os.totalmem(),
+    freeMem:os.freemem()
+}
+
+console.log(currentOS)
